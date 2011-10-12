0bbe293949d17064f71f4a7ec19b89b5bf060ae3
Dan Sully <daniel-github@electricrain.com>
2011-10-12 06:15:07 -0700

    Migrate to PircBotX & multi-server support.

--- src/main/scala/org/sully/Derpbird.scala
+++ src/main/scala/org/sully/Derpbird.scala
@@ -1,61 +1,79 @@
 package org.sully
 
-import org.apache.commons.configuration.{Configuration, PropertiesConfiguration, CompositeConfiguration}
-import org.jibble.pircbot.PircBot
-import twitter4j.Status
-import twitter4j.TwitterException
-import twitter4j.TwitterFactory
+import java.io.IOException
 
 import actors.Actor
+import collection.mutable.HashMap
 
-class Derpbird extends PircBot {
+import twitter4j.{Status, TwitterException, TwitterFactory}
 
-  var channels = collection.mutable.Set[String]()
-  var nick     = ""
-  var finger   = ""
+import org.apache.commons.configuration.HierarchicalINIConfiguration
+
+import org.pircbotx.{MultiBotManager, PircBotX, UtilSSLSocketFactory}
+import org.pircbotx.exception.{IrcException, NickAlreadyInUseException}
+import org.pircbotx.hooks.{Listener, ListenerAdapter}
+import org.pircbotx.hooks.events._
+import org.pircbotx.hooks.managers.ThreadedListenerManager
+
+class MyINIConfiguration(filename: String) extends HierarchicalINIConfiguration(filename) {
+
+  // Override to not count '#' as a comment character.
+  val COMMENT_CHARS = ";"
+
+  // HierarchicalINIConfiguration doesn't actually split properly.
+  override def getStringArray(key: String): Array[String] = {
+
+    for (value:String <- getString(key).split(","))
+      yield value.trim
+  }
+}
+
+class Derpbird[T <: PircBotX] extends ListenerAdapter[T] with Listener[T] {
+
+  var configs = new HashMap[String, HierarchicalINIConfiguration]
 
   // Regexes to match input from IRC.
   val sup_match = """^sup (.+)""".r
   val ext_match = """twitter.com/.+/statuses/(\d+)$""".r
 
-  def initialize() {
-    TwitterFetch.start()
-
-    setLogin(nick)
-    setName(nick)
-    setFinger(finger)
-    //startIdentServer()
+  // Keep the config around so we can use it to rejoin channels on reconnect.
+  def addConfig(server: String, config: HierarchicalINIConfiguration) {
+    configs += server -> config
   }
 
-  // Keep a set of channels to use on reconnect.
-  def addChannel(channel: String) {
-    channels += channel
+  def joinChannels(event: ConnectEvent[T]) {
+
+    val server = event.getBot.getServer
+    val config = configs.get((server)).get
+
+    for (channelSection <- config.getSection(server).getString("channels").split(",")) {
+      val channelConfig = config.getSection(channelSection.trim)
+
+      val channel  = channelConfig.getString("channel")
+      val password = channelConfig.getString("password", null)
+
+      println("Joining channel: " + channel + " on: " + server + ":" + event.getBot.getPort)
+      event.getBot.joinChannel(channel, password)
+    }
   }
 
-  def joinChannels() {
-    channels.foreach(channel => println("onConnect: joining channel: " + channel))
-    channels.foreach(channel => joinChannel(channel))
-  }
-
-  override def onConnect() {
-    println("Connected to: " + getServer + ":" + getPort)
-    println(channels)
-    channels.foreach(channel => joinChannel(channel))
-    joinChannels()
+  override def onConnect(event: ConnectEvent[T]) {
+    println("Connected to: " + event.getBot.getServer + ":" + event.getBot.getPort)
+    joinChannels(event)
   }
 
   // Re-connect to the server if we've been dropped.
-  override def onDisconnect() {
-    println("Disconnected from: " + getServer + ":" + getPort + ", attempting to reconnect in 5 seconds!")
+  override def onDisconnect(event: DisconnectEvent[T]) {
+    println("Disconnected from: " + event.getBot.getServer + ":" + event.getBot.getPort + ", attempting to reconnect in 5 seconds!")
 
     var attempts = 0
 
     Thread.sleep(5 * 1000)
 
-    while (!isConnected && attempts < 15) {
+    while (!event.getBot.isConnected && attempts < 15) {
       try {
         attempts += 1
-        reconnect()
+        event.getBot.reconnect()
       } catch {
         case e: Exception => {
           Thread.sleep(10 * 1000)
@@ -64,22 +82,22 @@ class Derpbird extends PircBot {
     }
   }
 
-  override def onKick(channel: String, nick: String, login: String, hostname: String, kickee: String, reason: String) {
+  override def onKick(event: KickEvent[T]) {
 
-    if (kickee.equalsIgnoreCase(getNick)) {
-      println("Kicked from channel: " + channel + ", sleeping for 5 seconds before rejoining..")
+    if (event.getRecipient.getNick == event.getBot.getNick) {
+      println("Kicked from channel: " + event.getChannel.getName + ", sleeping for 5 seconds before rejoining..")
       Thread.sleep(5 * 1000)
-      joinChannel(channel)
+      event.getBot.joinChannel(event.getChannel.getName)
     }
   }
 
-  override def onMessage(channel: String, sender: String, login: String, hostname: String, message: String) {
+  override def onMessage(event: MessageEvent[T]) {
 
     try {
 
-      message match {
-        case sup_match(username) => TwitterFetch ! TwitterMessageFetchForUser(username, this, channel)
-        case ext_match(tweetId)  => TwitterFetch ! TwitterMessageFetchForId(tweetId, this, channel)
+      event.getMessage match {
+        case sup_match(username) => TwitterFetch ! TwitterMessageFetchForUser(event, username)
+        case ext_match(tweetId)  => TwitterFetch ! TwitterMessageFetchForId(event, tweetId)
         case _ => null
       }
 
@@ -92,8 +110,8 @@ class Derpbird extends PircBot {
 }
 
 // Actor matching.
-case class TwitterMessageFetchForUser(username: String, bot: Derpbird, channel: String);
-case class TwitterMessageFetchForId(id: String, bot: Derpbird, channel: String);
+case class TwitterMessageFetchForUser(event: MessageEvent[_ <: PircBotX], username: String)
+case class TwitterMessageFetchForId(event: MessageEvent[_ <: PircBotX], id: String)
 
 object TwitterFetch extends Actor {
 
@@ -101,9 +119,9 @@ object TwitterFetch extends Actor {
 
   def formatStatus(status: Status) = "<" + status.getUser.getScreenName + "> " + status.getText
 
-  def fetchTimeLineForUser(username: String, bot: Derpbird, channel: String) {
+  def fetchTimeLineForUser(event: MessageEvent[_ <: PircBotX], username: String) {
     try {
-      bot.sendMessage(channel, formatStatus(twitter.getUserTimeline(username).get(0)))
+      event.getBot.sendMessage(event.getChannel, formatStatus(twitter.getUserTimeline(username).get(0)))
 
     } catch {
       case te: TwitterException => {
@@ -112,9 +130,9 @@ object TwitterFetch extends Actor {
     }
   }
 
-  def fetchTweet(id: String, bot: Derpbird, channel: String) {
+  def fetchTweet(event: MessageEvent[_ <: PircBotX], id: String) {
     try {
-      bot.sendMessage(channel, formatStatus(twitter.showStatus(id.toLong)))
+      event.getBot.sendMessage(event.getChannel, formatStatus(twitter.showStatus(id.toLong)))
 
     } catch {
       case te: TwitterException => {
@@ -128,8 +146,8 @@ object TwitterFetch extends Actor {
 
     loop {
       react {
-        case TwitterMessageFetchForUser(username, bot, channel) => fetchTimeLineForUser(username, bot, channel)
-        case TwitterMessageFetchForId(tweetId, bot, channel) => fetchTweet(tweetId, bot, channel)
+        case TwitterMessageFetchForUser(event, username) => fetchTimeLineForUser(event, username)
+        case TwitterMessageFetchForId(event, tweetId) => fetchTweet(event, tweetId)
         case _ => null
       }
     }
@@ -140,33 +158,55 @@ object Main {
 
   def main(args: Array[String]) {
 
-    val config = new CompositeConfiguration()
-
-    if (args.length > 0)
-      config.append(new PropertiesConfiguration(args(0)))
-
-    config.append(defaultConfiguration())
-
-    val bot = new Derpbird()
-
-    bot.setVerbose(config.getBoolean("verbose", true))
-    bot.nick = config.getString("nick", "derpbird")
-    bot.finger = config.getString("finger", "herp derp")
-
-    for (channel <- config.getStringArray("channels")) {
-      bot.addChannel(channel)
+    if (args.length == 0) {
+      println("Usage: <derpbird> config.ini")
+      System.exit(1)
     }
 
-    bot.initialize()
+    // Fire up the Actor that will send requests to Twitter.
+    TwitterFetch.start()
+
+    val config   = new MyINIConfiguration(args(0))
+    val multi    = new MultiBotManager("Derpbird")
+    val manager  = new ThreadedListenerManager
+    val listener = new Derpbird
+
+    // Associate our Derpbird class with the PircBotX manager.
+    manager.addListener(listener)
+
+    multi.setEncoding("UTF-8")
+    multi.setListenerManager(manager)
+
+    for (serverKey <- config.getStringArray("global.servers")) {
+
+      val server   = config.getSection(serverKey)
+      val host     = server.getString("host")
+      val port     = server.getInt("port", 6667)
+      val pass     = server.getString("password", null)
+      val socket   = if (server.getBoolean("ssl", false)) new UtilSSLSocketFactory else null
+
+      val bot      = multi.createBot(host, port, pass, socket)
+
+      bot.setName(server.getString("nick", "derpbird"))
+      bot.setLogin(server.getString("nick", "derpbird"))
+      bot.setFinger(server.getString("finger", "herp derp"))
+      bot.setVersion(server.getString("version", "Derpbird"))
+      bot.setVerbose(server.getBoolean("verbose", true))
+
+      // My identification? You don't need to see my identification.
+      if (server.getString("nickpass", null) != null) {
+        bot.identify(server.getString("nickpass"))
+      }
+
+      listener.addConfig(serverKey, config)
+    }
 
     try {
-      bot.connect(config.getString("host"), config.getInt("port"))
+      multi.connectAll()
     } catch {
-      case e: java.net.ConnectException => {
-        println("Couldn't connect to IRC Server: " + bot.getServer + ":" + bot.getPort)
+      case e: (IOException, IrcException, NickAlreadyInUseException) => {
+        println("Couldn't connect to all servers: " + e.toString)
       }
     }
   }
-
-  private def defaultConfiguration() : Configuration = new PropertiesConfiguration(Main.getClass.getResource("default.properties"))
 }
